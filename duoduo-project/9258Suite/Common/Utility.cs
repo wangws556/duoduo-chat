82a04c9b2f6cf587174cbe0f620286951975c1a9
Vincent wang <wenswang@microsoft.com>
2019-10-20 17:17:13 +0800

    update

--- duoduo-project/9258Suite/Common/Utility.cs
+++ duoduo-project/9258Suite/Common/Utility.cs
@@ -335,11 +335,11 @@ namespace YoYoStudio.Common
                 pro.StartInfo.Verb = "runas";
                 if (isSync)
                 {
-                    pro.StartInfo.Arguments = " -probesize 1024 -sync ext " + arg1 + " -autoexit";
+                    pro.StartInfo.Arguments = " -probesize 1024 -sync ext " + arg1 + " -nodisp -autoexit";
                 }
                 else
                 {
-                    pro.StartInfo.Arguments = " -probesize 1024 " + arg1 + "  -autoexit";
+                    pro.StartInfo.Arguments = " -probesize 1024 " + arg1 + " -nodisp -autoexit";
                 }
                 
                 try
