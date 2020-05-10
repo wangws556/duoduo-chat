434e886f2a28ec297f8539fc568c0d98d05bc6c0
Vincent wang <wenswang@microsoft.com>
2020-05-10 10:37:17 +0800

    update

---
+++ duoduo-project/9258Suite/Client.Chat/ValueConverters/AudioControlVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using YoYoStudio.Client.ViewModel;
+
+namespace YoYoStudio.Client.Chat.ValueConverters
+{
+    public class AudioControlVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            UserViewModel uvm = value as UserViewModel;
+            var result = Visibility.Hidden;
+            if (uvm != null)
+            {
+                if (uvm.IsMe())
+                {
+                    result = Visibility.Visible;
+                }
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
